7cafdfaf97bdb21ce374ea34120068b7e486f322
Deffinitiveee <xlegendsz131@gmail.com>
2025-04-07 05:33:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import a2s
+import os
 
 app = Flask(__name__)
 CORS(app)  # <- Esta línea habilita CORS
@@ -24,4 +25,5 @@ def server_info():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
